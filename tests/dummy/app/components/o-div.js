c0d12bbacf29625e2238399659ce21a8bc21f8e9
James H. Hill <james@onehilltech.com>
2019-03-27 23:33:52 -0400

    chore: misssing files

---
+++ tests/dummy/app/components/o-div.js
@@ -0,0 +1,10 @@
+import Component from '@ember/component';
+import layout from '../templates/components/o-div';
+
+export default Component.extend({
+  layout,
+
+  classNames: ['o-div'],
+
+  customProperties: ['backgroundImage']
+});
