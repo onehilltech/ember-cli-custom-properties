688fddd8d529e807e449ad62bb43aba9dd2ea05f
James Hill <james@onehilltech.com>
2020-04-28 11:58:24 -0400

    feat: added modifier to support 2.17

---
+++ tests/integration/modifiers/custom-property-test.js
@@ -0,0 +1,15 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Modifier | custom-property', function(hooks) {
+  setupRenderingTest(hooks);
+
+  // Replace this with your real tests.
+  test('it renders', async function(assert) {
+    await render(hbs`<div {{custom-property}}></div>`);
+
+    assert.ok(true);
+  });
+});
