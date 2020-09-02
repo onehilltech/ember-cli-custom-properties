ae31ec72705d2c5ebab54fe14bd3d93338625301
James Hill <james@onehilltech.com>
2020-09-02 00:45:45 -0400

    fix: undefined values should remove the property

--- addon/modifiers/custom-property.js
+++ addon/modifiers/custom-property.js
@@ -1,5 +1,11 @@
 import { modifier } from 'ember-modifier';
+import { isPresent } from '@ember/utils';
 
 export default modifier (function customProperty (element, [prop, value] /*, hash*/) {
-  element.style.setProperty (prop, value);
+  if (isPresent (value)) {
+    element.style.setProperty (prop, value);
+  }
+  else {
+    element.style.removeProperty (prop, value);
+  }
 });
